5a36bedf5925a69f6ce27724323d49dabdfdcdd8
Christian Leroy <christian.leroy@gmx.de>
2026-03-25 12:45:59 +0100

    refactoring of test class

--- scripts/test_scripts.py
+++ scripts/test_scripts.py
@@ -9,8 +9,8 @@ class TestCleanup(unittest.TestCase):
     """Class for testing cleanup.py"""
     def test_isOver(self):
         static_today = dt.date(2026, 7, 2)
-        self.assertTrue(isOver("2026-07-01", static_today), "2026-07-01 is before 2026-07-02")
-        self.assertFalse(isOver("2026-07-02", static_today), "2026-07-02 is NOT before 2026-07-02")
+        self.assertEqual(isOver("2026-07-01", static_today), True, "2026-07-01 is before 2026-07-02")
+        self.assertEqual(isOver("2026-07-02", static_today), False, "2026-07-02 is NOT before 2026-07-02")
 
 
 class TestExportToIcal(unittest.TestCase):
@@ -22,27 +22,27 @@ class TestExportToIcal(unittest.TestCase):
                         "postal_code": "72072",
                         "city": "Tübingen"
                         }
-        self.assertTrue(createLocation(test_milonga) == "Tanz-Atelier, Provenceweg 22, 72072 Tübingen")
+        self.assertEqual(createLocation(test_milonga), "Tanz-Atelier, Provenceweg 22, 72072 Tübingen")
 
     def test_uid_has_no_spaces(self):
         test_milonga = {"title": "Eine Test-Milonga", "date": "2026-07-02"}
-        self.assertTrue(createUid(test_milonga) == "2026-07-02-Eine-Test-Milonga@tangotuebingen.de")
+        self.assertEqual(createUid(test_milonga), "2026-07-02-Eine-Test-Milonga@tangotuebingen.de")
 
     def test_uid_has_no_special_char(self):
         test_milonga = {"title": "!Eine Test-Milongä & Practica", "date": "2026-07-02"}
-        self.assertTrue(createUid(test_milonga) == "2026-07-02-Eine-Test-Milong-Practica@tangotuebingen.de")
+        self.assertEqual(createUid(test_milonga), "2026-07-02-Eine-Test-Milong-Practica@tangotuebingen.de")
 
 
 class TestGenerateEvents(unittest.TestCase):
     """Class for testing generate_events.py"""
     def testNextDay(self):
-        self.assertTrue(daysUntilWeekday(6,6) == 7) # Sunday to Sunday: 7 days
-        self.assertTrue(daysUntilWeekday(6,0) == 1) # Sunday to Monday: 1 day
-        self.assertTrue(daysUntilWeekday(6,5) == 6) # Sunday to Friday: 6 days 
+        self.assertEqual(daysUntilWeekday(6,6), 7) # Sunday to Sunday: 7 days
+        self.assertEqual(daysUntilWeekday(6,0), 1) # Sunday to Monday: 1 day
+        self.assertEqual(daysUntilWeekday(6,5), 6) # Sunday to Friday: 6 days
 
-        self.assertTrue(daysUntilWeekday(0,0) == 7) # Monday to Monday: 7 days
-        self.assertTrue(daysUntilWeekday(0,1) == 1) # Monday to Tuesday: 1 day
-        self.assertTrue(daysUntilWeekday(1,0) == 6) # Monday to Sunday: 6 days
+        self.assertEqual(daysUntilWeekday(0,0), 7) # Monday to Monday: 7 days
+        self.assertEqual(daysUntilWeekday(0,1), 1) # Monday to Tuesday: 1 day
+        self.assertEqual(daysUntilWeekday(1,0), 6) # Monday to Sunday: 6 days
 
 
 if __name__ == "__main__":
